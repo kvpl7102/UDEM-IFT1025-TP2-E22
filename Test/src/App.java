d92cbcd1ee10c5fcaf9ae6b31418ab5337701ba8
kinhvi_pl <86048815+kvpl7102@users.noreply.github.com>
2022-07-07 18:08:30 -0700

    Progress 7/7

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,10 +1,12 @@
+
 public class App {
     public static void main(String[] args) {
-        Course course1 = new Course("IFT", 1025);
+
+        // CoursesList coursesList = new CoursesList();
+
+        // Course course1 = new Course("IFT", 1025);
 
         // course1.setCourseDate();
-        System.out.println(course1.getCourseDate());
-        // System.out.println(course1.getCourseHours());
 
     }
 }
---
+++ Test/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    
+    private String name;
+    private int id;
+    
+    Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
